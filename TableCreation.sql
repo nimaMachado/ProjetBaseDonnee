31fd40540277870db22513b2816d77d5a42f64d6
Anatole MOUQUET <anatole mouquet@yahoo.com>
2023-04-05 09:42:08 +0200

    a

--- TableCreation.sql
+++ TableCreation.sql
@@ -1,113 +1,115 @@
-CREATE TABLE IF NOT EXIST Client(
+CREATE TABLE IF NOT EXISTS Client(
     id serial,
     nom text,
     prenom text,
     email text,
-    CONSTRAINT pk_client PRIMARY KEY id
+    id_geo integer,
+    CONSTRAINT pk_client PRIMARY KEY (id),
+    CONSTRAINT hk_client_geo FOREIGN KEY (id_geo) REFERENCES Region_geo(id)
 );
 
-CREATE TABLE IF NOT EXIST Liquid(
+CREATE TABLE IF NOT EXISTS Liquid(
     id serial,
     ispotable BOOLEAN,
     nom text,
-    CONSTRAINT pk_liquid PRIMARY KEY id
+    CONSTRAINT pk_liquid PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Region_geo(
+CREATE TABLE IF NOT EXISTS Region_geo(
     id serial,
     nom text,
-    CONSTRAINT pk_region_geo PRIMARY KEY id
+    CONSTRAINT pk_region_geo PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Fournisseur(
+CREATE TABLE IF NOT EXISTS Fournisseur(
     id serial,
     nom text,
-    CONSTRAINT pk_fournisseur PRIMARY KEY id
-)
+    CONSTRAINT pk_fournisseur PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXIST Subvention(
+CREATE TABLE IF NOT EXISTS Subvention(
     id serial,
     nom text,
     date_début DATE,
     date_fin DATE,
-    CONSTRAINT pk_subvention PRIMARY KEY id
+    CONSTRAINT pk_subvention PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Pays(
+CREATE TABLE IF NOT EXISTS Pays(
     id integer,
-    CONSTRAINT hk_pays FOREIGN KEY Pays(id) REFERENCES Region_geo(id),
-    CONSTRAINT pk_pays PRIMARY KEY id
+    CONSTRAINT hk_pays FOREIGN KEY (id) REFERENCES Region_geo(id),
+    CONSTRAINT pk_pays PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Etatique(
+CREATE TABLE IF NOT EXISTS Etatique(
     id integer,
     id_pays_concerne integer,
-    CONSTRAINT hk_etatique FOREIGN KEY Etatique(id) REFERENCES Subvention(id),
-    CONSTRAINT fk_etatique_pays FOREIGN KEY Etatique(id_pays_concerne) REFERENCES Pays(id),
-    CONSTRAINT pk_etatique PRIMARY KEY id
+    CONSTRAINT hk_etatique FOREIGN KEY (id) REFERENCES Subvention(id),
+    CONSTRAINT fk_etatique_pays FOREIGN KEY (id_pays_concerne) REFERENCES Pays(id),
+    CONSTRAINT pk_etatique PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Region(
+CREATE TABLE IF NOT EXISTS Region(
     id integer,
-    CONSTRAINT hk_region FOREIGN KEY Region(id) REFERENCES Region_geo(id),
-    CONSTRAINT pk_region PRIMARY KEY id
+    CONSTRAINT hk_region FOREIGN KEY (id) REFERENCES Region_geo(id),
+    CONSTRAINT pk_region PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Regionale(
-    id integer,
-    id_region_concerne integer,
-    CONSTRAINT hk_regionale FOREIGN KEY Regionale(id) REFERENCES Subvention(id),
-    CONSTRAINT fk_regionale_region FOREIGN KEY Etatique(id_region_concerne) REFERENCES Region(id),
-    CONSTRAINT pk_regionale PRIMARY KEY id
-);
-
-CREATE TABLE IF NOT EXIST Metropole(
-    id integer,
-    CONSTRAINT hk_metropole FOREIGN KEY Metropole(id) REFERENCES Region_geo(id),
-    CONSTRAINT pk_metropole PRIMARY KEY id
-);
-
-CREATE TABLE IF NOT EXIST Metropolitaine(
+CREATE TABLE IF NOT EXISTS Regionale(
     id integer,
     id_region_concerne integer,
-    CONSTRAINT hk_metropolitaine FOREIGN KEY Regional(id) REFERENCES Subvention(id),
-    CONSTRAINT fk_metropolitaine_metropole FOREIGN KEY Metropolitaine(id_region_concerne) REFERENCES Metropole(id),
-    CONSTRAINT pk_regional PRIMARY KEY id
+    CONSTRAINT hk_regionale FOREIGN KEY (id) REFERENCES Subvention(id),
+    CONSTRAINT fk_regionale_region FOREIGN KEY (id_region_concerne) REFERENCES Region(id),
+    CONSTRAINT pk_regionale PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Commune(
+CREATE TABLE IF NOT EXISTS Metropole(
     id integer,
-    CONSTRAINT hk_commune FOREIGN KEY Commune(id) REFERENCES Region_geo(id),
-    CONSTRAINT pk_commune PRIMARY KEY id
+    CONSTRAINT hk_metropole FOREIGN KEY (id) REFERENCES Region_geo(id),
+    CONSTRAINT pk_metropole PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Communale(
+CREATE TABLE IF NOT EXISTS Metropolitaine(
+    id integer,
+    id_region_concerne integer,
+    CONSTRAINT hk_metropolitaine FOREIGN KEY (id) REFERENCES Subvention(id),
+    CONSTRAINT fk_metropolitaine_metropole FOREIGN KEY (id_region_concerne) REFERENCES Metropole(id),
+    CONSTRAINT pk_regional PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Commune(
+    id integer,
+    CONSTRAINT hk_commune FOREIGN KEY (id) REFERENCES Region_geo(id),
+    CONSTRAINT pk_commune PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Communale(
     id integer,
     id_commune_concerne integer,
-    CONSTRAINT hk_communale FOREIGN KEY Communale(id) REFERENCES Subvention(id),
-    CONSTRAINT fk_communale_commune FOREIGN KEY Communal(id_commune_concerne) REFERENCES Commune(id),
-    CONSTRAINT pk_communale PRIMARY KEY id
+    CONSTRAINT hk_communale FOREIGN KEY (id) REFERENCES Subvention(id),
+    CONSTRAINT fk_communale_commune FOREIGN KEY (id_commune_concerne) REFERENCES Commune(id),
+    CONSTRAINT pk_communale PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Sub_valFix(
+CREATE TABLE IF NOT EXISTS Sub_valFix(
     id integer,
-    val_const integer DEFAULT 0 CHECK val_mult >= 0,
-    val_mult integer DEFAULT 0 CHECK val_mult >= 0 and val_mult <= 1,
-    CONSTRAINT hk_sub_valFix FOREIGN KEY Sub_valFix(id) REFERENCES Subvention(id),
-    CONSTRAINT pk_sub_valfix PRIMARY KEY id
+    val_const integer DEFAULT 0 CHECK (val_mult >= 0),
+    val_mult integer DEFAULT 0 CHECK (val_mult >= 0 and val_mult <= 1),
+    CONSTRAINT hk_sub_valFix FOREIGN KEY (id) REFERENCES Subvention(id),
+    CONSTRAINT pk_sub_valfix PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Sub_valHab(
+CREATE TABLE IF NOT EXISTS Sub_valHab(
     id integer,
-    val_const integer DEFAULT 0 CHECK val_mult >= 0,
-    val_mult integer DEFAULT 0 CHECK val_mult >= 0 and val_mult <= 1,
-    val_upperCap integer NOT NULL CHECK val_upperCap > 0,
-    val_lowerCap integer DEFAULT 0 CHECK val_lowerCap > 0,
-    CONSTRAINT hk_sub_valhab FOREIGN KEY Sub_valHab(id) REFERENCES Subvention(id),
-    CONSTRAINT pk_sub_valhab PRIMARY KEY id
+    val_const integer DEFAULT 0 CHECK (val_mult >= 0),
+    val_mult integer DEFAULT 0 CHECK (val_mult >= 0 and val_mult <= 1),
+    val_upperCap integer NOT NULL CHECK (val_upperCap > 0),
+    val_lowerCap integer DEFAULT 0 CHECK (val_lowerCap > 0),
+    CONSTRAINT hk_sub_valhab FOREIGN KEY (id) REFERENCES Subvention(id),
+    CONSTRAINT pk_sub_valhab PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Eau(
+CREATE TABLE IF NOT EXISTS Eau(
     id integer,
     magnesium float NOT NULL,
     nitrates float NOT NULL,
@@ -119,30 +121,51 @@ CREATE TABLE IF NOT EXIST Eau(
     potassium float NOT NULL,
     silicum float NOT NULL,
     fluor float NOT NULL,
-    CONSTRAINT hk_eau FOREIGN KEY Eau(id) REFERENCES Liquid(id),
-    CONSTRAINT pk_eau PRIMARY KEY id
+    CONSTRAINT hk_eau FOREIGN KEY (id) REFERENCES Liquid(id),
+    CONSTRAINT pk_eau PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Jus(
+CREATE TABLE IF NOT EXISTS Jus(
     id integer,
     composition text,
-    CONSTRAINT hk_jus FOREIGN KEY Jus(id) REFERENCES Liquid(id),
-    CONSTRAINT pk_jus PRIMARY KEY id
+    CONSTRAINT hk_jus FOREIGN KEY (id) REFERENCES Liquid(id),
+    CONSTRAINT pk_jus PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXIST Alcool(
+CREATE TABLE IF NOT EXISTS Alcool(
     id integer,
     composition text,
     alcool_value integer,
-    CONSTRAINT hk_alcool FOREIGN KEY Alcool(id) REFERENCES Liquid(id),
-    CONSTRAINT pk_alcool PRIMARY KEY id
+    CONSTRAINT hk_alcool FOREIGN KEY (id) REFERENCES Liquid(id),
+    CONSTRAINT pk_alcool PRIMARY KEY (id)
 );
 
 /*      
 Debut des tables de liens.
 */
 
+CREATE TABLE IF NOT EXISTS Geo_sub_liquide(
+    id_region_concerne integer NOT NULL,
+    id_sub integer NOT NULL,
+    id_liquide integer NOT NULL,
+    CONSTRAINT hk_geo_sub_liquide__l FOREIGN KEY (id_liquide) REFERENCES Liquid(id),
+    CONSTRAINT hk_geo_sub_liquide__s FOREIGN KEY (id_sub) REFERENCES Subvention(id),
+    CONSTRAINT hk_geo_sub_liquide__r FOREIGN KEY (id_region_concerne) REFERENCES Region_geo(id)
+);
 
+CREATE TABLE IF NOT EXISTS Client_geo(
+    id_client integer NOT NULL,
+    id_region_concerne integer NOT NULL,
+    CONSTRAINT hk_client_geo__c FOREIGN KEY (id_client) REFERENCES Client(id),
+    CONSTRAINT hk_client_geo__r FOREIGN KEY (id_region_concerne) REFERENCES Region_geo(id)
+);
+
+CREATE TABLE IF NOT EXISTS Geo_geo(
+    id_geo integer NOT NULL,
+    id_geo_macro integer NOT NULL,
+    CONSTRAINT hk_geo_geo__g FOREIGN KEY (id_geo) REFERENCES Region_geo(id),
+    CONSTRAINT hk_geo_geo__mg FOREIGN KEY (id_geo_macro) REFERENCES Region_geo(id)
+);
 
 
 
